4a92cdf24ede08746ccd289716170a1bf9e57fc5
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-02-20 00:26:03 +0200

    create list view, implement adapter pattern

---
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -0,0 +1,46 @@
+package com.example.android.quakereport;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
+
+    public EarthquakeAdapter(Activity context, ArrayList<Earthquake> earthquakes){
+        super(context,0, earthquakes);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+       // return super.getView(position, convertView, parent);
+
+        View listItemView = convertView;
+        if (listItemView == null) {
+            listItemView = LayoutInflater.from(getContext()).inflate(
+                    R.layout.earthquake_list_item, parent,false);
+
+        }
+
+        Earthquake currentEarthquake = getItem(position);
+
+        TextView magnitudeView = (TextView) listItemView.findViewById(R.id.magnitude);
+        magnitudeView.setText(currentEarthquake.getMagnitude());
+
+        TextView locationView = (TextView) listItemView.findViewById(R.id.location);
+        locationView.setText(currentEarthquake.getLocation());
+
+        TextView dateView = (TextView) listItemView.findViewById(R.id.date);
+        dateView.setText(currentEarthquake.getDate());
+
+        return listItemView;
+    }
+}
