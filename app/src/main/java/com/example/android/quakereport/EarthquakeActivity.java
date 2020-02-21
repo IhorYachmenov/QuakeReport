09c1043bcec5aee11d05efccfd44f0f727498d6e
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-02-21 20:52:06 +0200

    add comments

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -31,7 +31,8 @@ public class EarthquakeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
+
+        //** Create a fake list of earthquake locations.
         ArrayList<Earthquake> earthquakes = new ArrayList<>();
         earthquakes.add(new Earthquake("8.9","San Francisco","02.02.2020"));
         earthquakes.add(new Earthquake("8.9","San Francisco","02.02.2020"));
