da08258dd98e2e1ddeb7456d5a4b3bab82a89dbf
IhorYachmenov <IlLoganlI@Hotmail.com>
2020-02-27 16:32:20 +0200

    create loader and empty text view for error loading

--- app/src/main/java/com/example/android/quakereport/Earthquake.java
+++ app/src/main/java/com/example/android/quakereport/Earthquake.java
@@ -1,11 +1,32 @@
+/*
+ * Copyright (C) 2016 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.example.android.quakereport;
 
+/**
+ * An {@link Earthquake} object contains information related to a single earthquake.
+ */
 public class Earthquake {
 
-    private double  mMagnitude;
+    /** Magnitude of the earthquake */
+    private double mMagnitude;
 
+    /** Location of the earthquake */
     private String mLocation;
 
+    /** Time of the earthquake */
     private long mTimeInMilliseconds;
 
     /** Website URL of the earthquake */
@@ -27,13 +48,20 @@ public class Earthquake {
         mUrl = url;
     }
 
+    /**
+     * Returns the magnitude of the earthquake.
+     */
     public double getMagnitude() {
         return mMagnitude;
     }
 
-    public String getLocation(){
+    /**
+     * Returns the location of the earthquake.
+     */
+    public String getLocation() {
         return mLocation;
     }
+
     /**
      * Returns the time of the earthquake.
      */
@@ -44,5 +72,7 @@ public class Earthquake {
     /**
      * Returns the website URL to find more information about the earthquake.
      */
-    public String getUrl() { return mUrl; }
+    public String getUrl() {
+        return mUrl;
+    }
 }
